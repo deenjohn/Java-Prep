efbac671af002bbf321347aec4d85dbe354cc513
Deen John <dj.itbhu@gmail.com>
2019-03-23 21:34:37 +0530

    Create 1-Source File Declaration Rules.java

---
+++ 1-Source File Declaration Rules.java
@@ -0,0 +1,18 @@
+
+
+/*
+There can be only one public class per source code file.
+*/
+public class Simple{  
+    public static void main(String args[]){  
+     char c = 65 ;
+     System.out.println("Hello Java");  
+     System.out.println(c);  
+    }  
+}
+
+public class SimpleTest{  
+    public static void test(String args[]){  
+     System.out.println("Hello SimpleTest");     
+    }  
+}
